8af40e425fd69198c914873c8b6b8a648a25120a
BerkeAnna <berke.anna0803@gmail.com>
2024-06-25 15:25:17 +0200

    .

---
+++ todoList/components/Task.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text, StyleSheet} from 'react-native';
+
+const Task = () => {
+
+    return(
+        //todo: https://www.youtube.com/watch?v=0kL6nhutjQ8  6:12
+    )
+}
