a2ae470d64ad19843f00a7c2156fb452a94204ce
BerkeAnna <berke.anna0803@gmail.com>
2024-06-26 09:08:10 +0200

    finish

--- todoList/App.js
+++ todoList/App.js
@@ -1,23 +1,63 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, Platform, Pressable, Keyboard  } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState('');
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    if (task) { // Check to avoid adding empty tasks
+      setTaskItems([...taskItems, task]);
+      setTask('');
+    }
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
   return (
     <View style={styles.container}>
-
-      {/* Today's Tasks*/}
+      {/* Today's Tasks */}
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
-        
         <View style={styles.items}>
-          {/* this is where the tasks will go */}
-          <Task text={'Task 1'}/>
-          <Task text={'Task 2'}/>
+          {/* This is where the tasks will go */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <Pressable  key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} />
+                </Pressable>
+              )
+            })
+          }
         </View>
-
       </View>
 
+      {/* Write a task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={'Write a task'}
+          value={task}
+          onChangeText={text => setTask(text)}
+        />
+
+        <Pressable onPress={handleAddTask}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </Pressable>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -26,7 +66,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#E8EAED',
-   
   },
   tasksWrapper: {
     paddingTop: 80,
@@ -39,4 +78,32 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
 });
---
+++ todoList/README.md
@@ -0,0 +1,3 @@
+
+https://www.youtube.com/watch?v=0kL6nhutjQ8
+https://www.youtube.com/watch?v=00HFzh3w1B8
